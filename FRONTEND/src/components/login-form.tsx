2fe39ec8c64da7e807f7fdc9b35c69e9c858fd54
user.name like:"akshar-1801 <user.email like:"aksharvadher001@gmail.com>
2025-01-17 22:24:42 +0530

    commit-100

--- FRONTEND/src/components/login-form.tsx
+++ FRONTEND/src/components/login-form.tsx
@@ -23,7 +23,7 @@ export function LoginForm({ setIsLoggedIn, ...props }: LoginFormProps) {
 
     try {
       const response = await axios.post(
-        "https://backend-f4c6g2izd-akshar-1801s-projects.vercel.app/login",
+        "https://backend-o2gk2g83r-akshar-1801s-projects.vercel.app/login",
         { phone, password }, // Axios automatically serializes JSON
         {
           headers: {
